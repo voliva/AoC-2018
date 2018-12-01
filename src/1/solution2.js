669cd71445b03e6c362fdfd54c2410a93b263541
Víctor Oliva <olivarra1@gmail.com>
2018-12-01 09:18:58 +0100

    Theoretical rxjs solution

--- src/1/solution2.js
+++ src/1/solution2.js
@@ -1,17 +1,28 @@
+// @ts-nocheck
+const { from } = require('rxjs');
+const {
+    filter,
+    map,
+    repeat,
+    scan,
+    take,
+    tap
+} = require('rxjs/operators');
 
-module.exports = inputLines => {
-    const numbers = inputLines.map(v => parseInt(v));
-
-    let frequency = 0;
-    let i=0;
-    const frequenciesFound = {};
-
-    do {
-        frequenciesFound[frequency] = true;
-        frequency += numbers[i];
-        i++;
-        i %= numbers.length;
-    } while(!frequenciesFound[frequency]);
-
-    return frequency;
-};
+module.exports = inputLines => from(inputLines).pipe(
+    map(v => parseInt(v)),
+    repeat(),
+    scan((acc, number) => ({
+        frequenciesFound: {
+            ...acc.frequenciesFounumber,
+            [acc.frequency]: true
+        },
+        frequency: acc.frequency + number
+    }), {
+        frequenciesFound: {},
+        frequency: 0
+    }),
+    filter(acc => acc.frequenciesFound[acc.frequency]),
+    take(1),
+    map(acc => acc.frequency)
+);
