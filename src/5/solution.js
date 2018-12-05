613988212633705f8aca0c5bccee4cb38a625fdc
Víctor Oliva <olivarra1@gmail.com>
2018-12-05 08:16:02 +0100

    Linked list

--- src/5/solution.js
+++ src/5/solution.js
@@ -1,32 +1,125 @@
-const { invertObj } = require('ramda');
+const { invertObj, compose, head } = require('ramda');
+
+const fromString = (str) => {
+    if(!str.length) return null;
+    const root = {
+        value: str[0],
+        previous: null,
+        next: null
+    };
+    let node = root;
+    for(let i=1; i<str.length; i++) {
+        node.next = {
+            value: str[i],
+            previous: node,
+            next: null
+        }
+        node = node.next;
+    }
+    return root;
+}
+
+const clone = list => {
+    if(!list) return null;
+    const root = {
+        ...list
+    };
+    let node = root;
+    for(; node.next; node = node.next) {
+        node.next = {
+            ...node.next
+        }
+    }
+    return root;
+}
+
+const getLength = (list) => {
+    if(!list) return 0;
+    let length = 1;
+    while(list.next) {
+        list = list.next;
+        length++;
+    }
+    return length;
+}
+
+const deleteNode = node => {
+    if(node.next) {
+        node.next.previous = node.previous;
+    }
+    if(node.previous) {
+        node.previous.next = node.next;
+    }
+}
+
+const getLCValueSet = list => {
+    const set = new Set();
+    for(; list; list = list.next) {
+        set.add(list.value.toLowerCase())
+    }
+    return set;
+}
 
 const react = (polymer, ignore) => {
-    let newString = '';
-    for(let i=0; i<polymer.length; i++) {
-        if(polymer[i].toLowerCase() === ignore) {
+    const initialLength = getLength(polymer);
+    let root = polymer;
+    for(; polymer; polymer = polymer.next) {
+        if(polymer.value.toLowerCase() === ignore) {
+            deleteNode(polymer);
+            if(!polymer.previous) {
+                root = polymer.next;
+            }
             continue;
         }
         if(
-            i+1 < polymer.length &&
-            polymer[i] !== polymer[i+1] &&
-            polymer[i].toLowerCase() === polymer[i+1].toLowerCase()
+            polymer.next &&
+            polymer.value !== polymer.next.value &&
+            polymer.value.toLowerCase() === polymer.next.value.toLowerCase()
         ) {
-            i++;
+            deleteNode(polymer);
+            if(!polymer.previous) {
+                root = polymer.next.next;
+            }
+            deleteNode(polymer.next);
+            polymer = polymer.next;
             continue;
         }
-        newString += polymer[i];
     }
 
-    if(newString.length === polymer.length) {
-        return newString;
+    const finalLength = getLength(root);
+    if(initialLength === finalLength) {
+        return root;
     }
 
-    return react(newString);
+    return react(root);
 }
 
-const solution1 = inputLines => react(inputLines[0]).length
+const solution1 = compose(
+    getLength,
+    react,
+    fromString,
+    head
+);
 
-const solution2 = inputLines => {
+const solution2 = compose(
+    polymer => {
+        const symbols = getLCValueSet(polymer);
+
+        let minimum = Infinity;
+        symbols.forEach(s => {
+            const rereduced = getLength(react(clone(polymer), s));
+
+            if(rereduced < minimum) {
+                minimum = rereduced;
+            }
+        });
+
+        return minimum;
+    },
+    react,
+    fromString,
+    head
+); inputLines => {
     const reduced = react(inputLines[0]);
     const symbols = Object.keys(invertObj(reduced.toLowerCase().split('')));
 
