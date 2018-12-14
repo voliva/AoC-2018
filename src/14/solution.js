4a8c1bd8f0975e430a291e3241f959506aac67f3
Víctor Oliva <olivarra1@gmail.com>
2018-12-14 06:30:37 +0100

    Solution to part 2

--- src/14/solution.js
+++ src/14/solution.js
@@ -1,4 +1,7 @@
 
+const endsWith = (target, arr) => target.every((t, i) => t == arr[arr.length - target.length + i]) ||
+    target.every((t, i) => t == arr[arr.length - target.length + i - 1]);
+
 const solution1 = inputLines => {
     const target = parseInt(inputLines[0]);
 
@@ -21,8 +24,27 @@ const solution1 = inputLines => {
 };
 
 const solution2 = inputLines => {
+    const target = '157901'.split('');
 
-    return inputLines;
+    const scores = [3,7];
+    let elves = [0,1];
+
+    while(!endsWith(target, scores)) {
+        const result = elves.reduce((acc, i) => acc + scores[i], 0);
+        if(result < 10) {
+            scores.push(result);
+        } else {
+            scores.push(Math.trunc(result/10));
+            scores.push(result % 10);
+        }
+        elves = elves.map(e => (e + scores[e] + 1) % scores.length);
+    }
+
+    if(target.every((t, i) => t == scores[scores.length - target.length + i])) {
+        return scores.length - target.length;
+    }
+
+    return scores.length - target.length - 1;
 };
 
 module.exports = [solution1, solution2];
