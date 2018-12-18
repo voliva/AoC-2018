8ab2d10c2cd10a9ee6af8b8dabe5b6cbf9ed86ea
Víctor Oliva <olivarra1@gmail.com>
2018-12-18 07:01:56 +0100

    Solution to part 2

--- src/18/solution.js
+++ src/18/solution.js
@@ -6,6 +6,20 @@ const parseArea = inputLines => {
     return area;
 }
 
+const printArea = area => {
+    const rows = area.map(r => r.join(''));
+    return rows.join('\n');
+}
+
+// const valueOfYard = c => c === '.' ? 0 : (c === '|' ? 1 : 2);
+const createHash = area => {
+    return printArea(area);
+    // Hash with collisions :/
+    // return area.reduce((acc, row, r) => acc + row.reduce((acc, char, c) => {
+    //     return acc + (r*row.length + c)*3 + valueOfYard(char);
+    // }, 0), 0);
+}
+
 const countAdjacent = (area, row, col) => {
     const counts = {
         '.': 0,
@@ -76,14 +90,56 @@ const solution1 = inputLines => {
     }
 
     const result = countAll(area);
-    console.log(result);
 
     return result['|'] * result['#'];
 };
 
 const solution2 = inputLines => {
+    let area = parseArea(inputLines);
 
-    return inputLines;
+    const scoreCache = {};
+    let previousHash = null;
+    let loopHash = null;
+    for(let i=0; i<10000000 && loopHash === null; i++) {
+        area = evolve(area);
+
+        const result = countAll(area);
+        const score = result['|'] * result['#'];
+        const hash = createHash(area);
+        if(previousHash !== null) {
+            scoreCache[previousHash].next = hash;
+        }
+        previousHash = hash;
+
+        if(scoreCache[hash]) {
+            loopHash = {i, hash};
+        } else {
+            scoreCache[hash] = {
+                score,
+                next: null
+            }
+        }
+    }
+
+    // Find out loop size
+    let loopSize = null;
+    let currentHash = scoreCache[loopHash.hash].next;
+    for(let i=1; i<loopHash.i && loopSize === null; i++) {
+        if(currentHash === loopHash.hash) {
+            loopSize = i;
+        }
+        currentHash = scoreCache[currentHash].next;
+    }
+
+    const remainingIterations = 1000000000 - loopHash.i - 1;
+    const loopIterations = remainingIterations % loopSize;
+
+    currentHash = loopHash.hash;
+    for(let i=0; i<loopIterations; i++) {
+        currentHash = scoreCache[currentHash].next;
+    }
+
+    return scoreCache[currentHash].score;
 };
 
 module.exports = [solution1, solution2];
