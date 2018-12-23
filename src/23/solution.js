e43d98585cf89ef2b0c12c53beae9991e07c09b1
Víctor Oliva <olivarra1@gmail.com>
2018-12-23 11:17:03 +0100

    Exponential solution to part2

--- src/23/solution.js
+++ src/23/solution.js
@@ -9,7 +9,7 @@ const parseBot = line => {
 }
 
 const getDistance = (b1, b2) => {
-    return b2.pos.reduce((d, v, i) => d + Math.abs(v - b1.pos[i]), 0);
+    return b2.reduce((d, v, i) => d + Math.abs(v - b1[i]), 0);
 }
 
 const solution1 = inputLines => {
@@ -22,10 +22,77 @@ const solution1 = inputLines => {
     });
 
     const inRange = bots.filter(bot =>
-        getDistance(bot, maxBot) <= maxBot.r
+        getDistance(bot.pos, maxBot.pos) <= maxBot.r
     );
 
     return inRange.length;
 }
 
-module.exports = [solution1];
+const getFromMap = (map, pos) => {
+    return map[pos.join(',')];
+}
+const setOnMap = (map, pos, v) => {
+    map[pos.join(',')] = v;
+}
+
+const kRot = 1 / Math.sqrt(2);
+const rotate = pos => {
+    return [
+        pos[0] * kRot - pos[1] * kRot,
+        pos[0] / 2 + pos[1] / 2 - pos[2] * kRot,
+        pos[0] / 2 + pos[1] / 2 + pos[2] * kRot
+    ]
+}
+const antirotate = pos => {
+    return [
+        pos[0] * kRot + pos[1] / 2 + pos[2] / 2,
+        -pos[0] * kRot + pos[1] / 2 + pos[2] / 2,
+        -pos[1] * kRot + pos[2] * kRot
+    ]
+}
+
+const kDist = 1 / Math.sqrt(3);
+const getPointAtDist = (dist, pos) => pos.map(v => v + dist * kDist);
+
+const intersect = (seg1, seg2) => {
+    const min = seg1.min.map((v,i) => Math.max(v, seg2.min[i]));
+    const max = seg1.max.map((v,i) => Math.min(v, seg2.max[i]));
+
+    if(min.some((m, i) => m >= max[i])) {
+        return null;
+    }
+    return { max, min }
+}
+
+const solution2 = inputLines => {
+    const bots = inputLines.map(parseBot);
+    const segments = [];
+
+    bots.forEach((bot, i) => {
+        console.log(i, segments.length);
+
+        const rotatedPosition = rotate(bot.pos);
+        const min = getPointAtDist(-bot.r, rotatedPosition);
+        const max = getPointAtDist(bot.r, rotatedPosition);
+
+        const segment = {
+            min,
+            max,
+            value: 1
+        }
+
+        segments.forEach(seg => {
+            const intersection = intersect(seg, segment);
+            if(intersection) {
+                intersection.value = seg.value + 1;
+                segments.push(intersection);
+            }
+        });
+
+        segments.push(segment);
+    });
+
+    return segments.length;
+}
+
+module.exports = [solution1, solution2];
